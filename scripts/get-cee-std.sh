3a77cac4eec499e5af1e115dee156826b6d774eb
mlite <mlite@users.noreply.github.com>
2021-09-29 10:09:17 -0700

    add a script to get cee.[hc]

---
+++ scripts/get-cee-std.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+mypath=$(dirname $(readlink -f $0))
+url="https://raw.githubusercontent.com/cee-studio/cee-std/master"
+
+wget $url/scripts/get-cee-std.sh -O ${mypath}/get-cee-std.sh
+chmod +x ${mypath}/get-cee-std.sh
+
+list="
+cee.h
+cee.c"
+
+mkdir -p $mypath/../cee-std
+pushd $mypath/../cee-std
+for i in $list; do
+    echo "getting $i"
+    echo "$url/release/$i"
+    wget $url/release/$i -O $i
+done
+popd
