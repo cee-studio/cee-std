93b62dbc271ccf22d22a4cb0dee964112ed11ef4
mlite <mlite@users.noreply.github.com>
2021-10-02 04:51:05 -0700

    disable cache

--- scripts/get-cee-std.sh
+++ scripts/get-cee-std.sh
@@ -5,7 +5,7 @@ set -o pipefail
 mypath=$(dirname $(readlink -f $0))
 url="https://raw.githubusercontent.com/cee-studio/cee-std/master"
 
-wget $url/scripts/get-cee-std.sh -O ${mypath}/get-cee-std.sh
+wget --no-cache $url/scripts/get-cee-std.sh -O ${mypath}/get-cee-std.sh
 chmod +x ${mypath}/get-cee-std.sh
 
 cee_list="
@@ -21,12 +21,12 @@ pushd $mypath/../cee-std
 for i in $cee_list; do
     echo "getting $i"
     echo "$url/release/$i"
-    wget $url/release/$i -O $i
+    wget --no-cache $url/release/$i -O $i
 done
 
 for i in $json_list; do
     echo "getting $i"
     echo "$url/cee-json/release/$i"
-    wget $url/cee-json/release/$i -O $i
+    wget --no-cache $url/cee-json/release/$i -O $i
 done
 popd
