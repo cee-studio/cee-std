a4764fb82114ab9cfb6f2e45ef39cc0b758808d5
LucasMull <muller.lucas@Hotmail.com>
2021-10-09 19:54:24 -0300

    chore(tester2.c): update

--- cee-json/test/tester2.c
+++ cee-json/test/tester2.c
@@ -7,7 +7,12 @@
 int main () {
   struct cee_state *state = cee_state_mk(100);
 
-  char *buf = "{ \"a\":-1, \"b\":5e10, \"c\":5e-10, \"d\":1.337 }";
+#if 0
+  char *buf = "{ \"a\":[[]], \"b\":5e10, \"c\":5e-10, \"d\":1.337, \"e\":-1 }";
+#else
+  char *buf = "{ \"f\":1, \"a\":[[]], \"b\":5e10, \"c\":5e-10, \"d\":1.337, \"e\":-1 }";
+#endif
+
   struct cee_json *result = NULL;
   int line = 0;
   if (!cee_json_parse(state, buf, strlen(buf),  &result, true, &line)) {
@@ -15,6 +20,6 @@ int main () {
     return 0;
   }
 
-  cee_json_asprint(state, &buf, result, 1);
+  cee_json_asprint(state, &buf, result, 0);
   fprintf(stdout, "%s\n", buf);
 }
