0f5a902ae5724f91e5bc360b7c84589ffcc2182c
LucasMull <muller.lucas@Hotmail.com>
2021-10-15 22:23:29 -0300

    feat(tester.c): convert a bunch more tests to greatest

--- test/tester.c
+++ test/tester.c
@@ -6,6 +6,16 @@
 #include "cee.h"
 #include "greatest.h"
 
+struct generic {
+  enum { UNDEF=0, INT32, STRING, FLOAT } type;
+  void *key;
+  union {
+    void *p;
+    int   i;
+  };
+};
+
+
 TEST check_string_against_original(char *str)
 {
   struct cee_state *st = cee_state_mk(10);
@@ -60,13 +70,110 @@ TEST check_list_heterogenous(void)
   struct cee_list *list = cee_list_mk(st, 10);
 
   /* heterogeneous list [ 10, 10.0, "10"] */
-  enum T { I_T, F_T, S_T };
-  cee_list_append(&list, cee_tagged_mk(st, I_T, cee_boxed_from_i32(st, 10)));
-  cee_list_append(&list, cee_tagged_mk(st, F_T, cee_boxed_from_float(st, 10.1f)));
-  cee_list_append(&list, cee_tagged_mk(st, S_T, cee_str_mk(st, "10")));
+  cee_list_append(&list, cee_tagged_mk(st, INT32, cee_boxed_from_i32(st, 10)));
+  cee_list_append(&list, cee_tagged_mk(st, FLOAT, cee_boxed_from_float(st, 10.1f)));
+  cee_list_append(&list, cee_tagged_mk(st, STRING, cee_str_mk(st, "10")));
   ASSERT_EQ(10, cee_boxed_to_i32(list->_[0]));
   ASSERT_EQ(10.1f, cee_boxed_to_float(list->_[1]));
-  ASSERT_STR_EQ("10", (char *)list->_[2]);
+  ASSERT_STR_EQ("10", (char*)list->_[2]);
+  cee_del(st);
+  PASS();
+}
+
+TEST check_set_find(char *str)
+{
+  struct cee_state *st = cee_state_mk(10);
+  struct cee_set *set = cee_set_mk(st, (cee_cmp_fun)&strcmp);
+  char *p;
+
+  cee_set_add(set, cee_str_mk(st, str));
+  p = cee_set_find(set, str);
+  ASSERT_STR_EQ(str, p);
+  cee_set_remove(set, cee_str_mk(st, str));
+  p = cee_set_find(set, str);
+  ASSERT(p == NULL);
+  cee_del(st);
+  PASS();
+}
+
+TEST check_set_values(char *str_list[], const unsigned n_str)
+{
+  struct cee_state *st = cee_state_mk(10);
+  struct cee_set *set = cee_set_mk(st, (cee_cmp_fun)&strcmp);
+  struct cee_list *set_values;
+  char *p;
+
+  for (unsigned i=0; i < n_str; ++i) {
+    cee_set_add(set, cee_str_mk(st, str_list[i]));
+  }
+
+  set_values = cee_set_values(set);
+  ASSERT(cee_set_size(set) == cee_list_size(set_values));
+
+  for (unsigned i=0; i < cee_list_size(set_values); ++i) {
+    p = cee_set_find(set, set_values->_[i]);
+    ASSERT(p != NULL);
+  }
+  cee_del(st);
+  PASS();
+}
+
+TEST check_map_find(struct generic list[], const unsigned n)
+{
+  struct cee_state *st = cee_state_mk(10);
+  struct cee_map *mp = cee_map_mk(st, (cee_cmp_fun)&strcmp);
+  struct cee_boxed *t;
+
+  for (unsigned i=0; i < n; ++i) {
+    cee_map_add(mp, cee_str_mk(st, list[i].key), cee_boxed_from_i32(st, list[i].i));
+  }
+  for (unsigned i=0; i < n; ++i) {
+    t = cee_map_find(mp, list[i].key);
+    ASSERT(t != NULL);
+  }
+  cee_del(st);
+  PASS();
+}
+
+#if 0
+void map_iter_cb(void *ctx, void *p_key, void *p_value)
+{
+  struct cee_str *key = p_key;
+  struct cee_boxed *value = p_value;
+  ASSERT(p_key != NULL);
+  ASSERT(p_value != NULL);
+}
+#endif
+
+TEST check_map_keys(struct generic list[], const unsigned n)
+{
+  struct cee_state *st = cee_state_mk(10);
+  struct cee_map *mp = cee_map_mk(st, (cee_cmp_fun)&strcmp);
+  struct cee_list *keys;
+
+  for (unsigned i=0; i < n; ++i) {
+    cee_map_add(mp, cee_str_mk(st, list[i].key), cee_boxed_from_i32(st, list[i].i));
+  }
+  keys = cee_map_keys(mp);
+  ASSERT(keys != NULL);
+  ASSERT_EQ(n, cee_list_size(keys));
+#if 0
+  /* @todo allow returning error value for cee_map_iterate() */
+  cee_map_iterate(mp, NULL, &map_iter_cb);
+#endif
+  cee_del(st);
+  PASS();
+}
+
+TEST check_map_overwrite(void)
+{
+  struct cee_state *st = cee_state_mk(10);
+  struct cee_map *mp = cee_map_mk(st, (cee_cmp_fun)&strcmp);
+
+  cee_map_add(mp, cee_str_mk(st, "1"), cee_boxed_from_i32(st, 10));
+  cee_map_add(mp, cee_str_mk(st, "1"), cee_boxed_from_i32(st, 100));
+  struct cee_boxed *t = cee_map_find(mp, "1");
+  ASSERT_EQ(100, cee_boxed_to_i32(cee_map_find(mp, "1")));
   cee_del(st);
   PASS();
 }
@@ -95,18 +202,11 @@ void * bar (struct cee_state *st, struct cee_env * outer, size_t amt, va_list ap
   return cee_boxed_from_i32(st, (i+j+k) * cee_boxed_to_i32(cee_closure_call(st, c, 3, 1, 2, 3)));
 }
 
-
-void f(void *ctx, void *key, void *value)
-{
-  struct cee_str *k = key;
-  struct cee_boxed *v = value;
-  printf ("key:%s->%d\n", k->_, cee_boxed_to_i32(v));
-}
-
 SUITE(cee_str)
 {
   char *str_list[] = { "Hello World!", "Fish", "" , "\n\t" };
-  for (int i=0; i < sizeof(str_list)/sizeof(char*); ++i) {
+
+  for (unsigned i=0; i < sizeof(str_list)/sizeof(char*); ++i) {
     RUN_TESTp(check_string_against_original, str_list[i]);
   }
   RUN_TEST(check_string_concatenation);
@@ -117,6 +217,31 @@ SUITE(cee_list)
   RUN_TEST(check_list_append);
 }
 
+SUITE(cee_set)
+{
+  char *str_list[] = { "Hello World!", "Fish", "" , "\n\t" };
+  const unsigned n_str = sizeof(str_list)/sizeof(char*);
+
+  for (unsigned i=0; i < n_str; ++i) {
+    RUN_TESTp(check_set_find, str_list[i]);
+  }
+  RUN_TESTp(check_set_values, str_list, n_str);
+}
+
+SUITE(cee_map)
+{
+  // type, key, value
+  struct generic list[] = {
+    { INT32, "1", (void*)10 },
+    { INT32, "2", (void*)20 },
+    { INT32, "3", (void*)30 }
+  };
+  const unsigned n_pairs = sizeof(list)/sizeof(struct generic);
+
+  RUN_TESTp(check_map_find, list, n_pairs);
+  RUN_TESTp(check_map_overwrite);
+}
+
 GREATEST_MAIN_DEFS();
 
 int main(int argc, char *argv[]) 
@@ -125,70 +250,10 @@ int main(int argc, char *argv[])
 
   RUN_SUITE(cee_str);
   RUN_SUITE(cee_list);
+  RUN_SUITE(cee_set);
+  RUN_SUITE(cee_map);
 
 #if 0
-  struct cee_state * st = cee_state_mk(10);
-  
-  /* test set */
-  struct cee_set * set1 = NULL;
-  set1 = cee_set_mk(st, (cee_cmp_fun)strcmp);
-  
-  printf ("st: %p\n", set1);
-  cee_set_add(set1, cee_str_mk(st, "a"));
-  cee_set_add(set1, cee_str_mk(st, "b"));
-  cee_set_add(set1, cee_str_mk(st, "c"));  
-  void * sx = cee_str_mk(st, "aabc");
-  printf ("sx:%p\n", sx);
-  cee_set_add(set1, sx);
-  
-  char * p = (char *)cee_set_find(set1, (char *)"aabc");
-  printf ("%p %s\n", p, p);
-  
-  cee_set_remove(set1, cee_str_mk(st, "aabc"));
-  p = (char *)cee_set_find(set1, (char *)"aabc");
-  printf ("%p\n", p);
-  
-  struct cee_list * svals = NULL;
-  svals = cee_set_values(set1);
-  printf ("print out values of a set\n");
-  for (i = 0; i < cee_list_size(svals); i++)
-    printf ("%d %s\n", i, (char*)svals->_[i]);
-  
-  // optional
-  // cee_del(set);
-  // cee_del(svals);
-  cee_state_add_gc_root(st, set1);
-  
-  /* test map */
-  struct cee_map * mp = NULL;
-  mp = cee_map_mk(st, (cee_cmp_fun)strcmp);
-  
-  cee_map_add(mp, cee_str_mk(st, "1"), cee_boxed_from_i32(st, 10));
-  cee_map_add(mp, cee_str_mk(st, "2"), cee_boxed_from_i32(st, 20));
-  cee_map_add(mp, cee_str_mk(st, "3"), cee_boxed_from_i32(st, 30));
-  cee_map_add(mp, cee_str_mk(st, "1"), cee_boxed_from_i32(st, 100));
-  
-  struct cee_boxed * t = (struct cee_boxed *)cee_map_find(mp, "1");
-  printf ("found value %d for key 1\n", cee_boxed_to_i32(t));
-  if (cee_boxed_to_i32(t) != 100)  {
-     printf("test failed\n");
-     cee_segfault();
-     return 1;
-  }
-
-
-  printf ("print out keys of a map\n");
-  struct cee_list * keys = cee_map_keys(mp);
-  for (i = 0; i < cee_list_size(keys); i++)
-    printf ("[%d] key:%s\n", i, (char *)keys->_[i]);
-
-  cee_map_iterate(mp, NULL, f);
-
-  // optional
-  //cee_del(keys);
-  //cee_del(mp);
-  cee_state_add_gc_root(st, mp);
-  
   /* test stack */
   struct cee_stack * sp = cee_stack_mk(st, 100);
   cee_stack_push(sp, cee_str_mk(st, "1"));
