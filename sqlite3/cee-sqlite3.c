1d3bd5c8329bbca5ecae6897ad6bfee718ac9f0d
mlite <mlite@users.noreply.github.com>
2021-11-17 04:10:25 -0800

    feat: check null

--- sqlite3/cee-sqlite3.c
+++ sqlite3/cee-sqlite3.c
@@ -280,7 +280,7 @@ dont_return_colum(struct cee_sqlite3_bind_info *info, char *colum_name)
 {
   int i = 0;
   for (i = 0; info[i].var_name; i++)
-    if (strcmp(info[i].col_name, colum_name) == 0)
+    if (info[i].col_name && strcmp(info[i].col_name, colum_name) == 0)
       return info[i].no_return;
   return false;
 }
