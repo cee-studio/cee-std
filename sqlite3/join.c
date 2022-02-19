5254240fc9f186de2fbd4e5cebba2be4c1580e6f
mlite <mlite@users.noreply.github.com>
2022-02-19 13:27:01 -0800

    feat: support join object/array

--- sqlite3/join.c
+++ sqlite3/join.c
@@ -1,14 +1,14 @@
 #include "cee-sqlite3.h"
 #include <stdlib.h>
-struct join_cxt {
+struct join_ctx {
   struct cee_sqlite3 *cs;
   char *sql;
   struct cee_sqlite3_bind_info *pairs;
 };
 
-static int join_one (void *cxt, int idx, void *elem) {
-  struct join_cxt *join_cxt = cxt;
-  struct cee_sqlite3_bind_info *pairs = join_cxt->pairs;
+static int join_one (void *ctx, int idx, void *elem) {
+  struct join_ctx *join_ctx = ctx;
+  struct cee_sqlite3_bind_info *pairs = join_ctx->pairs;
   struct cee_json *one = elem;
 
   int i;
@@ -29,22 +29,32 @@ static int join_one (void *cxt, int idx, void *elem) {
         cee_segfault();
     }
   }
-  cee_sqlite3_select1(join_cxt->cs, pairs, NULL, join_cxt->sql, &one);
+  cee_sqlite3_select1(join_ctx->cs, pairs, NULL, join_ctx->sql, &one);
   if (cee_json_select(one, ".error"))
     return 1;
   return 0;
 }
 
-void cee_sqlite3_join_json_table(struct cee_sqlite3 *cs,
-                                 struct cee_list *list,
-                                 struct cee_sqlite3_bind_info *pairs,
-                                 char *sql) {
-  struct cee_sqlite3_bind_info *info = NULL;
-  int i;
+void cee_sqlite3_json_array_join_table(struct cee_sqlite3 *cs,
+                                       struct cee_json *json,
+                                       struct cee_sqlite3_bind_info *pairs,
+                                       char *sql) {
+  struct join_ctx join_ctx = {0};
+  join_ctx.cs = cs;
+  join_ctx.sql = sql;
+  join_ctx.pairs = pairs;
+  struct cee_list *list = cee_json_to_array(json);
+  cee_list_iterate(list, &join_ctx, join_one);
+}
 
-  struct join_cxt join_cxt = {0};
-  join_cxt.cs = cs;
-  join_cxt.sql = sql;
-  join_cxt.pairs = info;
-  cee_list_iterate(list, &join_cxt, join_one);
+
+void cee_sqlite3_json_object_join_table(struct cee_sqlite3 *cs,
+                                        struct cee_json *json,
+                                        struct cee_sqlite3_bind_info *pairs,
+                                        char *sql) {
+  struct join_ctx join_ctx = {0};
+  join_ctx.cs = cs;
+  join_ctx.sql = sql;
+  join_ctx.pairs = pairs;
+  join_one(&join_ctx, 0, json);
 }
