2a29d2a0ae7339fbc1f1c22b2abf3d4a369627c1
mlite <mlite@users.noreply.github.com>
2023-04-13 22:13:45 -0700

    fix: the corner case of not full searched for possible replacement

--- release/cee.c
+++ release/cee.c
@@ -2173,7 +2173,7 @@ char* str_replace_all_ext(const char *str, size_t str_len, size_t *out_size, int
   while( i_src < str_len ){
     used_pair = NULL;
     for( int x = 0; x < n_pairs; x++ ){
-      if( i_src + pairs[x].old_len < str_len
+      if( i_src + pairs[x].old_len <= str_len
           && strncmp(str+i_src, pairs[x].old_needle, pairs[x].old_len) == 0 ){
         used_pair = pairs+x;
         break;
